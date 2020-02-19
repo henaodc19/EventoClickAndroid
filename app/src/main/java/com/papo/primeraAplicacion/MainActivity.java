d9a2a83abfa3951ad262c7aa35c35bae49b81345
henaodc19 <papo.dm19@gmail.com>
2020-02-18 21:12:47 -0500

    Primer commit

---
+++ app/src/main/java/com/papo/primeraAplicacion/MainActivity.java
@@ -0,0 +1,49 @@
+package com.papo.primeraAplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private Button btnAceptar1;
+    private Button btnAceptar2;
+
+
+    /**
+     * no es necesario ni declararlo ni intanciarlo
+     */
+    private Button btnAceptar3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponent();
+        onClickBtnLegacy();
+
+    }
+
+    private void initComponent(){
+        txtNombre = findViewById(R.id.txtNombre);
+        btnAceptar1 = findViewById(R.id.btnAceptar1);
+        btnAceptar2 = findViewById(R.id.btnAceptar2);
+    }
+
+    private void onClickBtnLegacy(){
+        btnAceptar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = txtNombre.getText().toString();
+                Toast.makeText(getApplicationContext(), nombre, Toast.LENGTH_SHORT).show();
+                txtNombre.setText("");
+            }
+        });
+    }
+}
